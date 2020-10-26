586d38b254223f7758edeffe8405b58e02202108
Leah Mattern <ladyleahmattern@gmail.com>
2020-10-26 14:49:15 -0400

    Adding layout components header and footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default (props) => {
+    <footer>
+        <p>
+            Copyright &copy; Leah Mattern All Rights Reserved
+            {' '}(new Date().getFullYear())
+        </p>
+    </footer>
+}
